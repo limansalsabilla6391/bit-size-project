032301e5f7bfe203658696b1eadfd8ae86b9a9e3
Diah Wahyudin <jessicalee4059@outlook.fr>
2026-02-11 19:16:58 +0000

    init project

---
+++ bit-size/src/test/java/com/example/bit_size/BitSizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bit_size;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitSizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
